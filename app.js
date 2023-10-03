53527779b24acb12bff4bd52468452de939e59a2
heybadrinath <ram.r@kalvium.community>
2023-10-03 18:24:25 +0530

    app.js

--- app.js
+++ app.js
@@ -1,86 +1,89 @@
-const battingbutton = document.getElementById("batting")
-const resetbutton = document.getElementById("restart")
-const total_scoreteam1 = document.getElementById("totalscore-team1")
-const total_scoreteam2 = document.getElementById("totalscore-team2")
-const wickets_team1 = document.getElementById("wickets-team1")
-const wickets_team2 = document.getElementById("wickets-team2")
+const strikeButton = document.getElementById("strike");
+const resetButton = document.getElementById("reset");
+const $team1Score = document.getElementById("score-team1");
+const $team1Wickets = document.getElementById("wickets-team1");
+const $team2Score = document.getElementById("score-team2");
+const $team2Wickets = document.getElementById("wickets-team2");
 
-var totalscore_team1 = 0;
-var totalscore_team2 = 0;
-var totalwickets_team1 = 0;
-var totalwickets_team2 = 0;
-var totalballs = 0;
-var totalballs2 = 0;
-var toss = 1;
+const strikeAudio = new Audio("http://bit.ly/so-ball-hit");
+const gameOverAudio = new Audio("http://bit.ly/so-crowd-cheer");
 
-const runs = ["W", 1, 2, 3, 4, 5, 6, 0]
+var team1Score = 0;
+var team1Wickets = 0;
+var team2Score = 0;
+var team2Wickets = 0;
+var team1BallsFaced = 0;
+var team2BallsFaced = 0;
+var turn = 1;
 
-resetbutton.onclick = () => {
-    window.location.reload();
+const possibleOutcomes = [0, 1, 2, 3, 4, 6, "W"];
+
+function gameOver() {
+  gameOverAudio.play();
+  if (team1Score > team2Score) alert("IND wins");
+  if (team2Score > team1Score) alert("PAK wins");
+  if (team2Score === team1Score) alert("It is another superover!");
 }
 
-
-function update(){
-    total_scoreteam1.textContent = totalscore_team1;
-    wickets_team1.textContent = totalwickets_team1;
-    total_scoreteam2.textContent = totalscore_team2;
-    wickets_team2.textContent = totalwickets_team2;
+function updateScore() {
+  $team1Score.textContent = team1Score;
+  $team1Wickets.textContent = team1Wickets;
+  $team2Score.textContent = team2Score;
+  $team2Wickets.textContent = team2Wickets;
 }
 
-function gameover(){
-    if(totalscore_team1>totalscore_team2){
-        alert("IND wins!")
-    }
-    if(totalscore_team2>totalscore_team1){
-        alert("PAK wins!")
-    }
-    if(totalscore_team1 == totalscore_team2){
-        alert("There is a tie")
-    }
-}
+resetButton.onclick = () => {
+  window.location.reload();
+};
 
+strikeButton.onclick = () => {
+  
+  strikeAudio.pause();
+  strikeAudio.currentTime = 0;
+  strikeAudio.play();
 
+  
+  const randomElement =
+    possibleOutcomes[Math.floor(Math.random() * possibleOutcomes.length)];
 
-battingbutton.onclick = () => {
-    const randomNum = runs[Math.floor(Math.random()*8)]
+  
+  if (turn === 2) {
     
-    if(toss === 2){
-        totalballs2++;
-        document.querySelector(`#team2score div:nth-child(${totalballs2})`).textContent = randomNum
-
-        if(randomNum === "W"){
-            totalwickets_team2++;
-        }else{
-            console.log(randomNum)
-            totalscore_team2 += randomNum
-
-            
-        }
-        if(totalballs2 == 6 || wickets_team2 == 2 || total_scoreteam2>total_scoreteam1){
-            toss = 3;
-            gameover();
-        }
+    team2BallsFaced++;
+    
+    document.querySelector(
+      `#team2-superover div:nth-child(${team2BallsFaced})`
+    ).textContent = randomElement;
+    
+    if (randomElement === "W") {
+      team2Wickets++;
     }
-    if(toss === 1){
-        totalballs++;
-        // console.log(totalballs)
-        document.querySelector(`#team1score div:nth-child(${totalballs})`).textContent = randomNum
-
-        if(randomNum === "W"){
-            totalwickets_team1++;
-        }else{
-            totalscore_team1 += randomNum
-
-            
-        }
-        if(totalballs == 6 || wickets_team1 == 2){
-            toss = 2;
-        }
+    
+    else {
+      team2Score += randomElement;
     }
-    update();
-   
+    
+    if (
+      team2BallsFaced === 6 ||
+      team2Wickets === 2 ||
+      team2Score > team1Score
+    ) {
+      turn = 3;
+      gameOver();
+    }
+  }
 
-
-
-
-}
+  if (turn === 1) {
+    team1BallsFaced++;
+    document.querySelector(
+      `#team1-superover div:nth-child(${team1BallsFaced})`
+    ).textContent = randomElement;
+    if (randomElement === "W") {
+      team1Wickets++;
+    } else {
+      team1Score += randomElement;
+    }
+    if (team1BallsFaced === 6 || team1Wickets === 2) turn = 2;
+  }
+  updateScore();
+};
